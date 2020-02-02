2f3e7773462e65801bb42dad18dee855830885dc
mr.mazurmaciej@gmail.com <mr.mazurmaciej@gmail.com>
2020-02-02 15:30:44 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './page/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div className="App">
-      <p>TEST</p>
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
     </div>
   );
 }
