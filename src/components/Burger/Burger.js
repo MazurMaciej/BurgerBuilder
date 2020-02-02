b13689b0c8345a0584d2c539857bef69f837c212
mr.mazurmaciej@gmail.com <mr.mazurmaciej@gmail.com>
2020-02-02 19:09:53 +0000

    Ingridient mechanism

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,12 @@ import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = (props) => {
-  let transformedIngredients = Object.keys(props.ingredients)
-    .map(igKey => {
-      return [...Array(props.ingredients[igKey])].map((_, i) => {
-        return <BurgerIngredient key={igKey + i} type={igKey} />;
-      });
-    })
+const burger = ({ ingredients }) => {
+  let transformedIngredients = Object.keys(ingredients).map(igKey => {
+    return [...Array(ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  })
 
   return (
     <div className={classes.Burger}>
