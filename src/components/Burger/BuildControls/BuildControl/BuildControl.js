0dc444a2ef17c45ff127a141f158ff70b6371db8
mr.mazurmaciej@gmail.com <mr.mazurmaciej@gmail.com>
2020-02-03 19:27:52 +0000

    Added function with added ingredients to burger

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import classes from './BuildControl.module.css';
 
-const buildControl = ({ label }) => (
-  <div>
-    <div>{label}</div>
-    <button>Less</button>
-    <button>More</button>
+const buildControl = ({ label, added }) => (
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}>{label}</div>
+    <button className={classes.Less}>Less</button>
+    <button className={classes.More} onClick={added}>More</button>
   </div>
 );
 
--- src/page/BurgerBuilder/BurgerBuilder.js
+++ src/page/BurgerBuilder/BurgerBuilder.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
+
   state = {
     ingredients: {
       salad: 0,
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 4
   }
+
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updateCounted = oldCount + 1;
+    const updateIngredients = {
+      ...this.state.ingredients
+    };
+    updateIngredients[type] = updateCounted;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({ totalPrice: newPrice, ingredients: updateIngredients });
+  };
+
+  removeIngredientHandler = (type) => {
+
+  };
+
 
   render() {
     return (
       <>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+        />
       </>
     );
   }
